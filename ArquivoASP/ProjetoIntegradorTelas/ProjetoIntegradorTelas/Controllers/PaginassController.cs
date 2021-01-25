e21b0a741415bc7bd3bdcb6a69c5c0480c9a74d4
Klaus Augustus Ramos Reiniger <klaus.reiniger@academico.ifrn.edu.br>
2021-01-25 20:27:49 -0300

    arquivo .net

---
+++ ArquivoASP/ProjetoIntegradorTelas/ProjetoIntegradorTelas/Controllers/PaginassController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoIntegradorTelas.Controllers
+{
+    public class PaginassController : Controller
+    {
+        // GET: Paginass
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
