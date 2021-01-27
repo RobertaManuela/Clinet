79d059c2c9ad2f807e0a01ae534b5bc1f4ae9c1d
Klaus Augustus Ramos Reiniger <klaus.reiniger@academico.ifrn.edu.br>
2021-01-27 12:54:45 -0300

    implementada versão inicial: página de contato, comandos razor adicionados

--- ArquivoASP/ProjetoIntegradorTelas/ProjetoIntegradorTelas/Controllers/PaginassController.cs
+++ ArquivoASP/ProjetoIntegradorTelas/ProjetoIntegradorTelas/Controllers/PaginassController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjetoIntegradorTelas.Models;
 
 namespace ProjetoIntegradorTelas.Controllers
 {
     public class PaginassController : Controller
     {
+        private static IList<contatoform> mensagens = new List<contatoform>();
         // GET: Paginass
         public ActionResult Index()
         {
@@ -27,8 +29,14 @@ namespace ProjetoIntegradorTelas.Controllers
             return View();
         }
 
-
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult contato(contatoform ctt) {
+            string message = Request.Form["Mensagem"];
+            ctt.mensagem = message;
+            mensagens.Add(ctt);
+            return RedirectToAction("Index");
+        }
 
     }
 }
